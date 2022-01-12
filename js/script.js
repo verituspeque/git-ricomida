99a4d8681ce9199fde17aa28ae70d6f64dc654ec
verituspeque <v_marelli@hotmail.com>
2022-01-12 16:09:52 -0300

    git commit -m "mi primer commit"

---
+++ js/script.js
@@ -0,0 +1,24 @@
+$(function() {
+$("#enviarCorreo").click(function () {
+alert("¡El correo ha sido enviado exitosamente!")
+
+})
+
+$(".card-title").click(function (){
+$(".card-text").toggle();
+})
+
+$("body").on("dblclick", "h5", function (){
+  $(this).css("color", "red");
+})
+
+
+
+
+
+
+
+
+
+
+})
